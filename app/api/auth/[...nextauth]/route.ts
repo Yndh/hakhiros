4a50942b3b765a593208a9d60e83c4cbfd12af9d
Qłesuga <110699693+Qlesuga@users.noreply.github.com>
2023-10-20 20:07:16 +0200

    register

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,13 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { prisma } from "@/lib/prisma";
+import { hash } from 'bcrypt'
 
 export const authOptions: NextAuthOptions = {
     providers: [
         CredentialsProvider({
-            name: 'Sign in',
+            id: "login",
+            name: 'logowanie',
             credentials: {
                 email: { label: "Email", type: "email", placeholder: "sigma@basedeparment.com" },
                 password: { label: "Hasło", type: "password" }
@@ -13,7 +16,41 @@ export const authOptions: NextAuthOptions = {
                 const user = { id: "1", name: "Giga Chad B)", email: "sigma@basedeparment.com" }
                 return user
             }
-        })
+        }),
+        CredentialsProvider({
+            id: "register",
+            name: 'rejstracja',
+            credentials: {
+                name: { label: "Nazwa", type: "string", placeholder: "sigma" },
+                email: { label: "Email", type: "email", placeholder: "sigma@basedeparment.com" },
+                password: { label: "Hasło", type: "password" }
+            },
+            async authorize(credentials, req) {
+                if (!credentials?.name || !credentials?.email || !credentials?.password) {
+                    return null
+                }
+
+                const emailExists = await prisma.user.findUnique({
+                    where: {
+                        email: credentials.email
+                    }
+                })
+
+                if (emailExists) {
+                    return null
+                }
+                const password: string = (await hash(credentials.password, 12)).toString()
+                const user = await prisma.user.create({
+                    data: {
+                        name: credentials.name,
+                        email: credentials.email,
+                        password
+                    },
+                });
+
+                return { id: user.id.toString(), name: user.name, email: user.email }
+            }
+        }),
     ]
 }
 
