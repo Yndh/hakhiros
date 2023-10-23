5ee11f6467504a575367d098f22fb46f09ffa1e4
Qłesuga <110699693+Qlesuga@users.noreply.github.com>
2023-10-23 23:55:15 +0200

    generate unique house code

---
+++ lib/generateCode.ts
@@ -0,0 +1,9 @@
+export function generateCode(length: number): string {
+    let result = '';
+    const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
