ef4479a70c1ba0dcc634b60177135aba6291f157
Marcin <m.szablak30@gmail.com>
2023-10-25 22:59:59 +0200

    calendar rework

--- app/app/plan/page.tsx
+++ app/app/plan/page.tsx
@@ -1,9 +1,38 @@
 import { AppLayout } from "@/app/components/appLayout"
+import "./style.css";
+
+const date = new Date()
+const day = date.getDay()
+const days = ["poniedziałek","wtorek","środa","czwartek","piatek","sobota","niedziela"]
 
 export default function Plan() {
-    return(
+    const user = ["maciek"];
+    const events = [{text:"aaa", startTime:"8:00",endTime:"9:00"}]
+
+    const hours = Array.from({ length: 25 }, (_, index) => index);
+
+    return (
         <AppLayout active="plan">
-            <h1>Test</h1>
+            <div className="main">
+                <div className="header">
+                    <h1>{days[day]}</h1>
+                </div>
+                <div className="content">
+                    {user.map((user) => 
+                        <ul className="user">
+                            {events.map((event) =>
+                                <li className="userEvent">{event.text}</li>
+                            )}
+                        </ul>
+                    )}
+                    {hours.map((hour) => (
+                        <div className="hour-line" key={hour}>
+                            <div className="hour-label">{hour.toString().padStart(2, '0')}:00</div>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </AppLayout>
-    )
-}
+    );
+};
+
