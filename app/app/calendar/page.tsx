4be48526414e9acb292cef608d2bff19b3833fbf
Adrian <114053643+AdrianPrzybylskiTIE9@users.noreply.github.com>
2023-10-26 13:52:51 +0200

    Add Note Sorting

--- app/app/calendar/page.tsx
+++ app/app/calendar/page.tsx
@@ -1,18 +1,14 @@
-"use client"
+"use client";
 
-import { AppLayout } from "@/app/components/appLayout"
-import React from 'react'
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
+import { AppLayout } from "@/app/components/appLayout";
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
 
 export default function Duties() {
-    return(
-        <AppLayout active="duties">
-        <FullCalendar
-          plugins={[
-            dayGridPlugin,
-          ]}
-        />
-        </AppLayout>
-    )
+  return (
+    <AppLayout active="calendar">
+      <FullCalendar plugins={[dayGridPlugin]} />
+    </AppLayout>
+  );
 }
