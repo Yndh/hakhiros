c75d2e2a4d6872ec080e9e7350969e19683c24cd
Qłesuga <110699693+Qlesuga@users.noreply.github.com>
2023-10-27 22:51:10 +0200

    small fixs

--- lib/joinHouse.ts
+++ lib/joinHouse.ts
@@ -2,12 +2,17 @@ import { createProfile } from "./createProfile";
 import { prisma } from "./prisma";
 
 export async function joinHouse(user_id: number, house_id: number) {
-    const house_exists = await prisma.house.findFirst({
+    const house = await prisma.house.findFirst({
+        select: {
+            name: true
+        },
         where: {
             id: house_id
         }
-    }).then(Boolean)
-
+    })
+    if (!house) {
+        return null
+    }
     const profile_id = await createProfile(user_id)
 
     const user_house = await prisma.user_house.create({
@@ -16,5 +21,5 @@ export async function joinHouse(user_id: number, house_id: number) {
             profile_id
         }
     })
-
+    return house.name
 }
