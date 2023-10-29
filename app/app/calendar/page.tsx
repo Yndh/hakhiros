c6b1d8e04455cbddc7162db249502bf70fda3b87
Marcin <m.szablak30@gmail.com>
2023-10-29 21:51:47 +0100

    calender update

--- app/app/calendar/page.tsx
+++ app/app/calendar/page.tsx
@@ -5,27 +5,59 @@ import React, { useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from '@fullcalendar/interaction'; 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClose } from "@fortawesome/free-solid-svg-icons";
 import "./style.css"
 
 export default function CalendarPage() {
-  const [open, setOpen] = useState(false)
-  const [selectedDate, setSelectedDate] = useState([{}])
+  const [openAdd, setOpenAdd] = useState(false)
+  const [openEdit, setOpenEdit] = useState(false)
+  const [selectedStartDate, setSelectedStartDate] = useState("");
+  const [selectedEndDate, setSelectedEndDate] = useState("");
   const [eventTitle, setEventTitle] = useState("")
-  const [eventList, setEventList] = useState({title: eventTitle,
-    start: "10.10.2023",
-    end: "11.10.2023",
-    display: 'background',})
+  const [eventsList, setEventsList] = useState([]);
+  const [eventChoosed, setEventChoosed] = useState({})
+
+  const setDate = (info) =>{
+    setSelectedStartDate(info.startStr)
+    setSelectedEndDate(info.endStr)
+  }
 
   const createEvent = (e) =>{
     e.preventDefault()
+    setOpenAdd(!openAdd)
+
     const newEvent ={
+      id: Math.random(),
       title: eventTitle,
-      start: selectedDate[0].toLocaleString(),
-      end: selectedDate[1].toLocaleString(),
-      display: 'background',
+      start: selectedStartDate.toLocaleString(),
+      end: selectedEndDate.toLocaleString(),
+      allDay: true,
+      color:"red",
     }
-    setEventList(newEvent)
-    console.log(eventList)
+
+    setEventTitle("")
+
+    const updatedEventsList = [...eventsList, newEvent];
+    setEventsList(updatedEventsList);
+  }
+
+  const editEvent = (event) =>{
+    //console.log(event)
+    const startDate = event.el.fcSeg.eventRange.range.start
+    const startDateStr = `${startDate.getFullYear()}-${String(startDate.getMonth() + 1).padStart(2, '0')}-${String(startDate.getDate()).padStart(2, '0')}`
+    const endDate = event.el.fcSeg.eventRange.range.end
+    const endDateStr = `${endDate.getFullYear()}-${String(endDate.getMonth() + 1).padStart(2, '0')}-${String(endDate.getDate()).padStart(2, '0')}`
+
+    setEventTitle(event.el.fcSeg.eventRange.def.title)
+    setSelectedStartDate(startDateStr)
+    setSelectedEndDate(endDateStr)
+  }
+
+  const deleteEvent = () =>{
+    console.log(eventChoosed)
+    const filteredEvents = eventsList.filter(item => item.id != eventChoosed.el.fcSeg.eventRange.def.publicId);
+    setEventsList(filteredEvents)
   }
 
   return(
@@ -36,37 +68,69 @@ export default function CalendarPage() {
               dayGridPlugin,
               interactionPlugin,
             ]}
-            events={eventList}
-            dateClick={()=> setOpen(!open)}
+            events={eventsList}
+            dateClick={()=> setOpenAdd(!openAdd)}
             locale = 'pl'
             selectable = {true}
-            select={(info) => setSelectedDate([info.startStr.valueOf, info.endStr.valueOf])}
+            select={(info) => setDate(info)}
+            eventClick={(event) => {editEvent(event); setOpenEdit(!openEdit); setEventChoosed(event)}}
           />
-          {open &&(
-            <div className="addEventContainer">
-              <div className="addEventBox">
-                <form className="editEvent">
-                  <label>Title</label>
+        </div>
+        {openAdd &&(
+            <div className="modal shown">
+              <div className="modalCard">
+                  <h2 className="title">Dodaj</h2>
+                  <label className="thin">tytuł</label>
                   <input type="text" value={eventTitle} onChange={(e) => setEventTitle(e.target.value)}/>
-                  <label>Data Początkowa</label>
+                  <label className="thin">Data Początkowa</label>
                   <input 
                     type="date" 
-                    value={selectedDate[0] && selectedDate[0].toLocaleString()}
-                    onChange={(e) => setSelectedDate([(e.target.value),selectedDate[1]])}
+                    value={selectedStartDate || ""}
+                    onChange={(e) => setSelectedStartDate(e.target.value)}
                   />
-                  <label>Data Końcowa</label>
+                  <label className="thin">Data Końcowa</label>
                   <input 
                     type="date" 
-                    value={selectedDate[1] && selectedDate[1].toLocaleString()}
-                    onChange={(e) => setSelectedDate([selectedDate[0], (e.target.value)])}
+                    value={selectedEndDate || ""}
+                    onChange={(e) => setSelectedEndDate(e.target.value)}
                   />
-                  <button onClick={(e) => createEvent(e)}/>
-                </form>
-                <p className="exitEvent" onClick={()=> setOpen(!open)}>x</p>
+                  <button onClick={(e) => createEvent(e)}>Dodaj</button>
+                  <FontAwesomeIcon
+                    icon={faClose}
+                    className="close"
+                    onClick={() => {setOpenAdd(!openAdd); setEventTitle("")}}
+                  />
               </div>
             </div>
           )}
-        </div>
+          {openEdit &&(
+            <div className="modal shown">
+              <div className="modalCard">
+                  <h2 className="title">Edytuj</h2>
+                  <label className="thin">tytuł</label>
+                  <input type="text" value={eventTitle} onChange={(e) => setEventTitle(e.target.value)}/>
+                  <label className="thin">Data Początkowa</label>
+                  <input 
+                    type="date" 
+                    value={selectedStartDate || ""}
+                    onChange={(e) => setSelectedStartDate(e.target.value)}
+                  />
+                  <label className="thin">Data Końcowa</label>
+                  <input 
+                    type="date" 
+                    value={selectedEndDate || ""}
+                    onChange={(e) => setSelectedEndDate(e.target.value)}
+                  />
+                  <button onClick={() => {deleteEvent(); setOpenEdit(!openEdit)}}>usuń</button>
+                  <button onClick={() => {}}>edytuj</button>
+                  <FontAwesomeIcon
+                    icon={faClose}
+                    className="close"
+                    onClick={() => {setOpenEdit(!openEdit); setEventTitle("")}}
+                  />
+              </div>
+            </div>
+          )}
       </AppLayout>
   )
 }
