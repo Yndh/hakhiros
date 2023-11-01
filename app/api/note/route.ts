c70367e0a6939ca9ed6fbeb54f855d60e9c2dc2f
Qłesuga <110699693+Qlesuga@users.noreply.github.com>
2023-11-01 12:34:59 +0100

    api delete note

--- app/api/note/route.ts
+++ app/api/note/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server'
 import type { NextApiResponse } from 'next'
 import { mPOST } from './POST'
 import { mGET } from './GET'
+import { mDELETE } from '../note/DELETE'
 
 
 export function POST(req: Request, res: NextApiResponse) {
@@ -10,4 +11,8 @@ export function POST(req: Request, res: NextApiResponse) {
 
 export function GET(req: Request, res: NextApiResponse) {
     return mGET(req, res)
+}
+
+export function DELETE(req: Request, res: NextApiResponse) {
+    return mDELETE(req, res)
 }
