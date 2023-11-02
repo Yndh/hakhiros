f7efa7764c833c2e37b4893c6e7f2dc7dc5c059a
Qłesuga <110699693+Qlesuga@users.noreply.github.com>
2023-11-02 13:25:34 +0100

    get note api

--- app/api/calendar/route.ts
+++ app/api/calendar/route.ts
@@ -1,6 +1,11 @@
 import type { NextApiResponse } from 'next'
 import { mPOST } from './POST'
+import { mGET } from './GET'
 
+
+export function GET(req: Request, res: NextApiResponse) {
+    return mGET(req, res)
+}
 
 export function POST(req: Request, res: NextApiResponse) {
     return mPOST(req, res)
