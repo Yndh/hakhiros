e10b616b8f3057b938a9e78ff4d78359f43234e6
Qłesuga <110699693+Qlesuga@users.noreply.github.com>
2023-11-05 13:21:27 +0100

    fix dutie POST

--- app/api/dutie/POST.ts
+++ app/api/dutie/POST.ts
@@ -6,7 +6,7 @@ import { NextResponse } from 'next/server'
 import type { NextApiResponse } from 'next'
 
 interface req_body {
-    house_id: number
+    house_id: number | string
     title: string
 }
 
@@ -20,29 +20,49 @@ export async function mPOST(req: Request, res: NextApiResponse) {
     }
     const body: req_body = await req.json()
 
-    if(!body.title || typeof body.title !== 'string'){
-        return new NextResponse(JSON.stringify({ error: 'wrong title field' }), {
+    if (!body.title || typeof body.title !== 'string') {
+        return new NextResponse(JSON.stringify({ error: 'zły tytuł' }), {
             status: 400
         })
     }
 
-    if(!body.house_id || typeof body.house_id !== 'number'){
-        return new NextResponse(JSON.stringify({ error: 'wrong house_id field' }), {
+    if (!body.house_id || !(typeof body.house_id === 'string' || typeof body.house_id === 'number')) {
+        return new NextResponse(JSON.stringify({ error: 'złe id domu' }), {
             status: 400
         })
     }
 
-    const dutie_add = await prisma.dutie.create({
-        data:{
-            house_id:body.house_id,
-            title:body.title,
-            is_done:false,
-            profile_id:session.user.id
+    const house_id = parseInt(body.house_id as string)
+    const profile = await prisma.user_house.findFirst({
+        select: {
+            profile_id: true
+        },
+        where: {
+            house_id
+        }
+    })
+    if (!profile) {
+        return new NextResponse(JSON.stringify({ error: 'nie znaleziono profilu' }), {
+            status: 400
+        })
+    }
+
+    const dutie = await prisma.dutie.create({
+        data: {
+            house_id: house_id,
+            title: body.title,
+            is_done: false,
+            profile_id: profile.profile_id
+        },
+        select: {
+            id: true,
+            title: true,
+            is_done: true
         }
     })
 
 
-    return new NextResponse(JSON.stringify({ dutie_add }), {
+    return new NextResponse(JSON.stringify({ dutie }), {
         status: 200
     })
 }
