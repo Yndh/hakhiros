71aea1a47165f0e6f35abc9565a4df68325efa9b
Qłesuga <110699693+Qlesuga@users.noreply.github.com>
2023-11-05 13:31:26 +0100

    fix dutie get method api

--- app/api/dutie/GET.ts
+++ app/api/dutie/GET.ts
@@ -3,13 +3,57 @@ import type { NextApiResponse } from 'next'
 import { prisma } from '@/lib/prisma'
 import { getServerSession } from 'next-auth/next'
 import { authOptions } from '../auth/[...nextauth]/route'
+import { getQuerryParameters } from '@/lib/getQuerryParameters'
 
 
 export async function mGET(req: Request, res: NextApiResponse) {
+    const session = await getServerSession(authOptions)
+    if (!session || !session.user) {
+        return new NextResponse(JSON.stringify({ error: 'nie zautoryzowany' }), {
+            status: 401
+        })
+    }
+    const querry_params = getQuerryParameters(req.url)
+    const user_house_id_param = querry_params.house_id
+    if (!user_house_id_param || (typeof user_house_id_param !== 'string' && typeof user_house_id_param !== 'number')) {
+        return new NextResponse(JSON.stringify({ error: 'nie poprawne id domu' }), {
+            status: 400
+        })
+    }
+    const user_house_id = parseInt(user_house_id_param as string)
 
-    const dutie = await prisma.dutie.findMany({})
+    const profile = await prisma.user_house.findFirst({
+        select: {
+            profile_id: true,
+            house_id: true
+        },
+        where: {
+            id: user_house_id,
+            profile: {
+                user_id: session.user.id
+            }
+        }
+    })
+    if (!profile) {
+        return new NextResponse(JSON.stringify({ error: 'nie znaleziono profilu' }), {
+            status: 400
+        })
+    }
+    const profile_id = profile.profile_id
 
-    return new NextResponse(JSON.stringify(dutie), {
+    const duties = await prisma.dutie.findMany({
+        select: {
+            id: true,
+            title: true,
+            is_done: true,
+        },
+        where: {
+            profile_id,
+            house_id: profile.house_id
+        }
+    })
+
+    return new NextResponse(JSON.stringify(duties), {
         status: 200
     })
 }
