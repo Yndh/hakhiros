02ffbb7fc9c09ff637f9e3d41a7b66aa40f64864
Yndh <przybylski-adrian@o2.pl>
2023-11-05 16:13:11 +0100

    Add Basic Branding

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,23 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Hakhiros apka wtf',
-  description: 'Next.js aplikacja hakhiros',
-}
+  title: "FamiLynk",
+  description: "Next.js aplikacja hakhiros",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/logo.svg" type="image/svg+xml" sizes="any" />
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
