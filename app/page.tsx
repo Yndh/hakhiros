b75806fb42836ea8ac6a355d743dbbc96c68f2eb
Yndh <przybylski-adrian@o2.pl>
2023-11-07 00:23:06 +0100

    Update Landing Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
-import "./landing.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./landing.css";
+import {
+  faQuestion,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import AppFunctions from "./components/appFunctions";
 
 export default function Home() {
   return (
@@ -9,16 +15,73 @@ export default function Home() {
           <p>FamiLynk</p>
         </div>
         <ul className="landingNav">
-          <li><a href="">Lorem</a></li>
-          <li><a href="">Ipsum</a></li>
-          <li><a href="">Lorem</a></li>
-          <li><a href="">Ipsum</a></li>
-          <li><a href=""><button>Zaloguj sie</button></a></li>
+          <li>
+            <a href="">Lorem</a>
+          </li>
+          <li>
+            <a href="/#about">O nas</a>
+          </li>
+          <li>
+            <a href="/#functions">Funkcje</a>
+          </li>
+          <li>
+            <a href="">Ipsum</a>
+          </li>
+          <li>
+            <a href="/app">
+              <button>Zaloguj sie</button>
+            </a>
+          </li>
         </ul>
       </header>
 
       <div className="heroContainer">
-      <a href="/app"><button>FamiLynk</button></a>
+        <h1>Twoje rodzinne centrum organizacji</h1>
+        <img src="/houseBaner.svg" alt="" />
+        {/* <a href="/app"><button>FamiLynk</button></a> */}
+      </div>
+
+      <div className="landingElement" id="about">
+        <h1>O nas</h1>
+        <div className="aboutUsRow">
+          <div className="aboutUsElement">
+            <FontAwesomeIcon icon={faUser}/>
+            <h3>O zespole</h3>
+            <p>
+              Jesteśmy zespołem pasjonatów, którzy wierzą, że harmonia w
+              rodzinie to klucz do szczęśliwego życia. Naszą misją jest pomóc
+              rodzinom organizować się, współpracować i celebrować wspólne
+              chwile.
+            </p>
+          </div>
+
+          <div className="aboutUsElement">
+          <FontAwesomeIcon icon={faQuestion}/>
+            <h3>Dlaczego FamiLynk?</h3>
+            <p>
+              Stworzyliśmy FamiLynk z przekonaniem, że każda rodzina zasługuje
+              na wyjątkowe narzędzie, które uprości jej życie i uczyni je
+              bardziej zorganizowanym. Nasza aplikacja powstała z myślą
+              o tych, którzy pragną spędzać więcej czasu z rodziną i mniej czasu
+              na zawiłościach organizacyjnych.
+            </p>
+          </div>
+
+          <div className="aboutUsElement">
+          <FontAwesomeIcon icon={faQuestion}/>
+            <h3>Co nas inspiruje?</h3>
+            <p>
+              Nasza inspiracja płynie z przekonania, że życie rodzinne może być
+              piękne, jeśli jest dobrze zorganizowane. Chcemy pomóc w tworzeniu
+              chwil pełnych uśmiechu i wspólnych doświadczeń.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="landingElement" id="functions">
+        <h1>Funkcje Aplikacji</h1>
+        <AppFunctions />
       </div>
     </main>
   );
