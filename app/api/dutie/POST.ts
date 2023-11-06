1d2d0312266f06348c0a4841c41c582ef0b126c3
Qłesuga <110699693+Qlesuga@users.noreply.github.com>
2023-11-06 19:21:44 +0100

    fix adding dutie on sunday

--- app/api/dutie/POST.ts
+++ app/api/dutie/POST.ts
@@ -39,8 +39,7 @@ export async function mPOST(req: Request, res: NextApiResponse) {
             status: 400
         })
     }
-
-    if (!body.week_day || !(typeof body.week_day === 'string' || typeof body.week_day === 'number')) {
+    if (body.week_day === undefined || !(typeof body.week_day === 'string' || typeof body.week_day === 'number')) {
         return new NextResponse(JSON.stringify({ error: 'zły dzień tygodnia' }), {
             status: 400
         })
