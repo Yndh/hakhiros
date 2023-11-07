070859318119248db6329bbd7a3666e2fb98f7a6
Yndh <przybylski-adrian@o2.pl>
2023-11-07 23:13:18 +0100

    Landing Page Changes

--- app/components/faq.tsx
+++ app/components/faq.tsx
@@ -6,29 +6,35 @@ const faqData = [
     question: "Czym są 'Domy'?",
     answer:
       "'Domy' w FamiLynk to grupy lub przestrzenie organizacyjne, które pozwalają na wspólne zarządzanie życiem rodzinym. Możesz utworzyć swój własny 'Dom' dla swojej rodziny lub dołączyć do istniejącego, aby wspólnie planować, organizować i zarządzać obowiązkami oraz wydarzeniami. Domy pozwalają na spersonalizowanie aplikacji pod potrzeby różnych grup rodzinnych.",
-    },
-    {
-      question:
+  },
+  {
+    question:
       "Czy FamiLynk jest dostępny na różne platformy, takie jak Android i iOS?",
-      answer:
-        "Tak, FamiLynk to aplikacja webowa, dostępna na różnych przeglądarkach internetowych, co oznacza, że możesz z niej korzystać zarówno na komputerze, jak i na urządzeniach mobilnych. Co najlepsze, FamiLynk jest dostępny w pełni za darmo, bez ukrytych opłat.",
-    },
+    answer:
+      "Tak, FamiLynk to aplikacja webowa, dostępna na różnych przeglądarkach internetowych, co oznacza, że możesz z niej korzystać zarówno na komputerze, jak i na urządzeniach mobilnych. Co najlepsze, FamiLynk jest dostępny w pełni za darmo, bez ukrytych opłat.",
+  },
   {
     question: "Jak mogę zacząć korzystać z aplikacji FamiLynk?",
     answer:
-      "Aby rozpocząć korzystanie z FamiLynk, wystarczy zarejestrować swoje konto, a następnie utworzyć 'Dom' lub dołączyć do istniejącego, gdzie będziesz mógł zarządzać życiem rodzinny wraz z innymi członkami.",
-    },
+      "Aby rozpocząć korzystanie z FamiLynk, wystarczy zarejestrować się, a następnie utworzyć lub dołączyć do 'Domu', gdzie będziesz mógł zarządzać życiem rodzinny wraz z innymi członkami.",
+  },
   {
     question:
       "Jak mogę zaprosić innych członków mojej rodziny do korzystania z aplikacji?",
     answer:
-      "Możesz zaprosić innych członków rodziny, udostępniając im link do dołączenia do 'Domu' lub podać im kod.",
+      "Możesz zaprosić ich udostępniając im link do dołączenia do 'Domu' lub podać im kod.",
   },
   {
     question:
-      "Jakie rodzaje obowiązków i wydarzeń można dodawać do kalendarza?",
+      "Czy mogę utworzyć wiele 'Domów' lub dołączyć do kilku różnych grup?",
     answer:
-      "Możesz dodawać różne rodzaje obowiązków i wydarzeń, takie jak spotkania, urodziny, zadania domowe, wyjścia i wiele innych. Nasza aplikacja pozwala na spersonalizowanie kalendarza według własnych potrzeb.",
+      "Tak, możesz zarządzać wieloma 'Domami' lub dołączyć do różnych grup w zależności od swoich potrzeb i sytuacji. To pozwala na spersonalizowanie aplikacji pod różne aspekty życia rodzinnego.",
+  },
+  {
+    question:
+      "Czy FamiLynk zbiera dane osobowe użytkowników i w jaki sposób są one chronione?",
+    answer:
+      "FamiLynk zbiera pewne dane osobowe użytkowników, takie jak adres e-mail i pseudonim. Ochrona tych danych jest dla nas priorytetem, i stosujemy odpowiednie środki bezpieczeństwa i zabezpieczenia, aby chronić prywatność użytkowników.",
   },
 ];
 
@@ -36,7 +42,12 @@ export default function FAQ() {
   return (
     <div className="faqColumn">
       {faqData.map((item, index) => (
-        <Expandable key={index} index={index+1} title={item.question} content={item.answer} />
+        <Expandable
+          key={index}
+          index={index + 1}
+          title={item.question}
+          content={item.answer}
+        />
       ))}
     </div>
   );
