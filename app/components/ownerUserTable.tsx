b50a9ae762d1e612704d8f410fdbc5f30e957995
Qłesuga <110699693+Qlesuga@users.noreply.github.com>
2024-01-30 23:20:16 +0100

    feat: display members name

--- app/components/ownerUserTable.tsx
+++ app/components/ownerUserTable.tsx
@@ -1,7 +1,11 @@
 import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default function OwnerUserTable() {
+interface OwnerUserTableProps {
+    members: Members
+}
+
+export default function OwnerUserTable({ members }: OwnerUserTableProps) {
     return <table className="users">
         <thead>
             <tr>
@@ -11,21 +15,17 @@ export default function OwnerUserTable() {
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>@user2137</td>
-                <td>31.10.2023</td>
-                <td>
-                    <FontAwesomeIcon icon={faRightFromBracket} className="kick" />
-                </td>
-            </tr>
-
-            <tr>
-                <td>@user2</td>
-                <td>31.10.2023</td>
-                <td>
-                    <FontAwesomeIcon icon={faRightFromBracket} className="kick" />
-                </td>
-            </tr>
+            {
+                Object.keys(members).map((key) => {
+                    return <tr key={key}>
+                        <td>@{members[key].name}</td>
+                        <td>31.10.2023</td>
+                        <td>
+                            <FontAwesomeIcon icon={faRightFromBracket} className="kick" />
+                        </td>
+                    </tr>
+                })
+            }
         </tbody>
     </table>
 }
--- app/components/userTable.tsx
+++ app/components/userTable.tsx
@@ -1,11 +1,27 @@
+import { useEffect, useRef } from "react";
 import OwnerUserTable from "./ownerUserTable"
+import useUserHouseId from "@/store/useUserHouseId";
+import { toast } from "react-toastify";
 
 interface UserTableProps {
     isOwner: boolean
 }
 
 export default function UserTable({ isOwner }: UserTableProps) {
+    const members = useRef<Members>({})
+    const user_house_id = useUserHouseId()
+    useEffect(() => {
+        fetch(`/api/members?user_house_id=${user_house_id}`)
+            .then((res) => res.json())
+            .then((data: Members | ErrorRespone) => {
+                if ("error" in data) {
+                    toast.error(`Wystąpił błąd: ${data["error"]}`);
+                    return;
+                }
+                members.current = data
+            });
+    }, [user_house_id])
     return <>{
-        isOwner ? <OwnerUserTable /> : <p>WIP</p>
+        isOwner ? <OwnerUserTable members={members.current} /> : <p>WIP</p>
     }</>
 }
